f67279778df18a90cf86a1b6a78a11e74308c6ac
Glen Sasek <glen.sasek@pcc.edu>
2025-02-17 19:30:42 +0000

    Working linear search

--- linear-search.py
+++ linear-search.py
@@ -1,20 +1,45 @@
 
-
+# ALGORITHM
+# Note: assume that list and key are already set
+# 1   set location = 1
+# 2   repeat until location > (length of the list)
+# 3       if list[location] == key
+# 4           print "Found at " + location
+# 5           end program
+# 6       set location = location + 1     advance to next location
+# 7
+# 8   print "Value not found"
 
 #Demo for Linear Search
 import random
 
 #main function
 def main():
-  value = 6;
+  find = 0
+  SIZE = 9
+  MIN = 1
+  MAX = 9
   numbers = []
-  for num in range(1, 10):
-    numbers.append(random.randrange(1, 10, 1))
-  print(numbers)
-  for num in range(1, len(numbers)):
-    if(numbers[num] == value):
-      print("Your number is in index: ", num)
-  if(num == len(numbers)-1):
+  indexes = []
+
+  find = int(input("ENTER A NUMBER 1-9: "))
+
+  for i in range(0, SIZE):
+    indexes.append(i)
+    numbers.append(random.randrange(MIN, MAX + 1))
+
+  print(" INDEX: ", indexes)  
+  print("NUMBER: ", numbers)
+
+  print ("Searching for: ", find)
+
+  i = 0
+  while(i < SIZE):
+    if(numbers[i] == find):
+      print("Your number is in index: ", i)
+      break
+    i += 1
+  else:
     print("Number not found!")
 
 #call main
